b46a028835855e4d66983956223b542d5ded1d10
Johnny Hausman <johnny@hausmanfamily.com>
2016-03-15 13:52:05 +0700

    *fix: import labels before the main controller.

--- assets/opstools/FCFActivities/FCFActivities.js
+++ assets/opstools/FCFActivities/FCFActivities.js
@@ -2,14 +2,14 @@ steal(
 // List your Page's dependencies here:
 // 'can/control/route'
 // ).then(
-	'opstools/FCFActivities/controllers/FCFActivities.js',
 	'opstools/FCFActivities/FCFActivities.css',
 	'opstools/FCFActivities/opsportal-theme-fcf.css',
+    'site/labels/opstool-FCFActivities.js',
 	function () {
 		System.import('appdev').then(function () {
             AD.ui.loading.resources(3);
 
-			steal.import('site/labels/opstool-FCFActivities').then(function () {
+			steal.import('opstools/FCFActivities/controllers/FCFActivities').then(function () {
 				AD.ui.loading.completed(3);
 			});
 		});
